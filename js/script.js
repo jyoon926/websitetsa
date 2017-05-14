776c5ba7e84dd534ceb7b0770c901ed0d57e470b
jyoon926 <jacobyoon926@gmail.com>
2017-05-13 22:07:09 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -511,3 +511,48 @@
   })();
 
 }).call(this);
+
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 50;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+var goUp = true;
+var end = null;
+var interval = null;
+
+function handle(delta) {
+	var animationInterval = 15; //lower is faster
+  var scrollSpeed = 25; //lower is faster
+
+	if (end == null) {
+  	end = $(window).scrollTop();
+  }
+  end -= 20 * delta;
+  goUp = delta > 0;
+
+  if (interval == null) {
+    interval = setInterval(function () {
+      var scrollTop = $(window).scrollTop();
+      var step = Math.round((end - scrollTop) / scrollSpeed);
+      if (scrollTop <= 0 ||
+          scrollTop >= $(window).prop("scrollHeight") - $(window).height() ||
+          goUp && step > -0.0001 ||
+          !goUp && step < 0.0001 ) {
+        clearInterval(interval);
+        interval = null;
+        end = null;
+      }
+      $(window).scrollTop(scrollTop + step );
+    }, animationInterval);
+  }
+}
